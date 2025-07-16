a383a9337f7bc840b09c68ce3ba159612da9ca7f
Richard de Vries <Pruyque@outlook.com>
2025-07-16 22:14:44 +0200

    Start of hardware interface

--- SoundSpeakGUI/Program.cs
+++ SoundSpeakGUI/Program.cs
@@ -1,7 +1,10 @@
 ﻿public class Program
 {
+    static SoundHardware? soundHardware = null;
     public static void Main(string[] args)
     {
-
+        soundHardware = new SoundHardware();
+        soundHardware.soundData += (args) => Console.WriteLine("test");
+        soundHardware.Begin();
     }
 }
